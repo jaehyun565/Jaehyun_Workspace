5c4a0d117d56fe1f44e4e5041bddc3f421a99a7f
천세경 <>
2024-04-24 09:51:09 +0900

    240424

---
+++ 0. live자료들/live-web-front/0423_day07_js_es6_promise/02_깊은복사/function-undefined.js
@@ -0,0 +1,15 @@
+const info = {
+  kor: '싸피',
+  year: {
+    classOf: 2019,
+  },
+  myFunc: function() {
+    console.log('?')
+  }
+}
+
+const newInfo = JSON.parse(JSON.stringify(info))
+
+console.log(info.myFunc)
+console.log(newInfo.myFunc) // undefined
+
